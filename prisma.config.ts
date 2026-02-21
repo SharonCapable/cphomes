9a122c6a24fdbc827efa2dd95172a90680ec8d0e
CirclePoint Developer <client@example.com>
2026-02-21 23:10:45 +0000

    Database migrated to Supabase

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,6 +7,6 @@ import { defineConfig } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: process.env.DIRECT_URL || process.env.DATABASE_URL,
+    url: process.env.DIRECT_URL || "postgresql://postgres:w7HjaHXMQGEgrDzx@db.ykyhuhyrenisaglzboqa.supabase.co:5432/postgres",
   },
 });
